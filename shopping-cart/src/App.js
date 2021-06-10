bbb30d337a4aa21d5596322026445bb45d728966
shaheen maqsood <shaheenmaqsood86@gmail.com>
2021-06-10 12:48:33 +0500

    first-commit

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shoppig Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All Rights Are Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
