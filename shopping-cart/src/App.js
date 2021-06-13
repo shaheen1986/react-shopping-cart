4089f3cc1c251bd7838f33fd631847516410581d
shaheen maqsood <shaheenmaqsood86@gmail.com>
2021-06-13 20:57:04 +0500

    checkout-form

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -9,14 +9,23 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : [],
       size: '',
       sort: '',
     };
   }
+  createOrder = (order) => {
+    alert(`Need to save order for ${order.name}`);
+  };
   removeFromCart = (item) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({ cartItems: cartItems.filter((x) => x._id !== item._id) });
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(cartItems.filter((x) => x._id !== item._id))
+    );
   };
   addToCart = (product) => {
     const cartItems = this.state.cartItems.slice();
@@ -31,6 +40,7 @@ class App extends React.Component {
       cartItems.push({ ...product, count: 1 });
     }
     this.setState({ cartItems });
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
   };
   sortProducts = (event) => {
     console.log(event.target.value);
@@ -92,6 +102,7 @@ class App extends React.Component {
               <Cart
                 cartItems={this.state.cartItems}
                 removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
               />
             </div>
           </div>
